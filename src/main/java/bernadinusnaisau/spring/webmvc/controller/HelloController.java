40823c80b00ff19c5e57f1550ffc93122ffdafab
bernad <bernadinusnaisau230803@gmail.com>
2025-09-02 23:31:41 +0700

    Belajar spring web mvc: Menentukan langsung get mapping tanpa perlu annotation @RequestMapping melainkan langsung @GetMapping untuk method GET dan dll

--- src/main/java/bernadinusnaisau/spring/webmvc/controller/HelloController.java
+++ src/main/java/bernadinusnaisau/spring/webmvc/controller/HelloController.java
@@ -5,7 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -16,7 +18,7 @@ public class HelloController {
     @Autowired
     private HelloService helloService;
 
-    @RequestMapping(path = "/hello")
+    @GetMapping(path = "/hello")
     public void helloController(HttpServletRequest request, HttpServletResponse response) throws IOException {
        String name = request.getParameter("name");
        String responBody  = helloService.hello(name);
