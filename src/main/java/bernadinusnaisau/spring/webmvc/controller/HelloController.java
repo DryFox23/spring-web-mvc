d1e332054e3d9bc75d6f2fa09661d51274739051
bernad <bernadinusnaisau230803@gmail.com>
2025-09-02 23:15:42 +0700

    Belajar spring web mvc: Pengubahan struktur HelloController

--- src/main/java/bernadinusnaisau/spring/webmvc/controller/HelloController.java
+++ src/main/java/bernadinusnaisau/spring/webmvc/controller/HelloController.java
@@ -1,7 +1,9 @@
 package bernadinusnaisau.spring.webmvc.controller;
 
+import bernadinusnaisau.spring.webmvc.service.HelloService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,12 +13,13 @@ import java.util.Objects;
 @Controller
 public class HelloController {
 
+    @Autowired
+    private HelloService helloService;
+
     @RequestMapping(path = "/hello")
     public void helloController(HttpServletRequest request, HttpServletResponse response) throws IOException {
        String name = request.getParameter("name");
-        if (Objects.isNull(name)){
-            name = "Guest";
-        }
-        response.getWriter().println("Hello " + name + "!");
+       String responBody  = helloService.hello(name);
+       response.getWriter().println(responBody);
     }
 }
