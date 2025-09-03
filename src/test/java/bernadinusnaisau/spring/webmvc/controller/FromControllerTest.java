7104133f49a978a140a7d156145de6d0b846fc5c
bernad <bernadinusnaisau230803@gmail.com>
2025-09-03 23:00:24 +0700

    Belajar spring web mvc: Testing memanggil post method url dengan menambahkan content type text html value

--- src/test/java/bernadinusnaisau/spring/webmvc/controller/FromControllerTest.java
+++ src/test/java/bernadinusnaisau/spring/webmvc/controller/FromControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +25,8 @@ public class FromControllerTest {
         mockMvc.perform(post("/hello/form")
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .queryParam("name", "Bernad"))
-                .andExpectAll(status().isOk(),(content().string(Matchers.containsString("Hello Bernad"))));
+                .andExpectAll(status().isOk(),
+                        header().string(HttpHeaders.CONTENT_TYPE, Matchers.containsString(MediaType.TEXT_HTML_VALUE)),
+                        (content().string(Matchers.containsString("Hello Bernad"))));
     }
 }
