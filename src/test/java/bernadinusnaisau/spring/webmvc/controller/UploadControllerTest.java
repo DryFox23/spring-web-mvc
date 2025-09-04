34a113e2ca8673999c8c0b00d6d2ea83aca05089
bernad <bernadinusnaisau230803@gmail.com>
2025-09-04 23:13:30 +0700

    Belajar spring web mvc: Testing upload file kedalam suatu directory menggunakan performnya MockMultiPartFile()

---
+++ src/test/java/bernadinusnaisau/spring/webmvc/controller/UploadControllerTest.java
@@ -0,0 +1,34 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.IOException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UploadControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void upload() throws Exception {
+        mockMvc.perform(multipart("/upload/profile")
+                .file(new MockMultipartFile("profile", "batman.png", "image/png", getClass().getResourceAsStream("/images/batman.png")))
+                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                .param("name", "bernad"))
+                .andExpectAll(status().isOk(), content()
+                        .string(Matchers.containsString("Success upload file: bernad to upload/batman.png")));
+    }
+}
