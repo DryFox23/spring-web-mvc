04397cf9416ae0712c637bc41315d3f9f816d32d
bernad <bernadinusnaisau230803@gmail.com>
2025-09-04 23:12:08 +0700

    Belajar spring web mvc: Implementasi upload file dengan consumes di post mappingnya yaitu multipart form data value serta annotation @RequestPart untuk mengambil data dari multipartnya

---
+++ src/main/java/bernadinusnaisau/spring/webmvc/controller/UploadController.java
@@ -0,0 +1,28 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Controller
+public class UploadController {
+
+    @PostMapping(path = "/upload/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public String upload(@RequestParam(name = "name")String name,
+                         @RequestPart(name = "profile")MultipartFile profile) throws IOException {
+        Path path = Path.of("upload/" + profile.getOriginalFilename());
+        String normalizePath = path.toString().replace("\\", "/");
+        profile.transferTo(path);
+        return "Success upload file: " +  name +  " to " +  normalizePath;
+
+    }
+}
