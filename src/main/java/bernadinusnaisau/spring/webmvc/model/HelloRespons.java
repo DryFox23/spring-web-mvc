a5f4f243b5fa672730084a757ec97a454355d441
bernad <bernadinusnaisau230803@gmail.com>
2025-09-05 18:55:22 +0700

    Belajar spring web mvc: Implementasi model untuk class hello respons sebagai respon dari kelas request nantinya

---
+++ src/main/java/bernadinusnaisau/spring/webmvc/model/HelloRespons.java
@@ -0,0 +1,13 @@
+package bernadinusnaisau.spring.webmvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloRespons {
+
+    private String hello;
+}
