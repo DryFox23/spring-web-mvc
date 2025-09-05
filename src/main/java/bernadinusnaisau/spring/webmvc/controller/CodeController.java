1844c3ca201a2413683e5e47fba4be065fceda80
bernad <bernadinusnaisau230803@gmail.com>
2025-09-05 19:22:05 +0700

    Belajar spring web mvc: Implementasi annotation @ResponseStatus untuk mengkostumisasi status dari Httpnya

---
+++ src/main/java/bernadinusnaisau/spring/webmvc/controller/CodeController.java
@@ -0,0 +1,18 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class CodeController {
+
+    @DeleteMapping(path = "/product/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    public void delete(@PathVariable("id") Integer id) {
+    }
+}
