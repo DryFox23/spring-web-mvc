d321c4369fae79e02a54d52e5f73fb47c907a89c
bernad <bernadinusnaisau230803@gmail.com>
2025-09-05 19:21:27 +0700

    Belajar spring web mvc: Testing memanggil code API controller menggunakan mockMvc dengan tipe perform delete

---
+++ src/test/java/bernadinusnaisau/spring/webmvc/controller/CodeControllerTest.java
@@ -0,0 +1,25 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class CodeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deleteAPI() throws Exception {
+        mockMvc.perform(delete("/product/23")
+        ).andExpectAll(status().isBadGateway());
+    }
+}
