b862a1d2e41cee36769bb49fda76d5e0f7092aba
bernad <bernadinusnaisau230803@gmail.com>
2025-09-05 21:03:53 +0700

    belajar-spring-web-mvc: Implementasi membuat cookie menggunakan HttoServletResponse dan mengambil data cookie menggunakan annotation @CookieValue

--- src/main/java/bernadinusnaisau/spring/webmvc/controller/AuthController.java
+++ src/main/java/bernadinusnaisau/spring/webmvc/controller/AuthController.java
@@ -1,9 +1,14 @@
 package bernadinusnaisau.spring.webmvc.controller;
 
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,12 +18,21 @@ public class AuthController {
     @PostMapping(path = "/auth/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public ResponseEntity<String> login(
             @RequestParam(name = "name") String name,
-            @RequestParam(name = "password") String password
-    ) {
+            @RequestParam(name = "password") String password,
+            HttpServletResponse response) throws Exception {
+
         if (name.equals("admin") && password.equals("rahasia")) {
+            Cookie cookie = new Cookie("name", name);
+            cookie.setPath("/");
+            response.addCookie(cookie);
             return new ResponseEntity<>("Welcome Admin!", HttpStatus.OK);
         }else {
             return new ResponseEntity<>("Invalid Credentials", HttpStatus.UNAUTHORIZED);
         }
+    }
+
+    @GetMapping(path = "/atuh/user")
+    public ResponseEntity<String>getUser(@CookieValue("name")String name) throws Exception {
+        return ResponseEntity.ok("Welcome Admin!" + name);
     }
 }
