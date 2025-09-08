2b68777196c985c376cb912619b2f0d624fc41c7
bernad <bernadinusnaisau230803@gmail.com>
2025-09-08 20:36:38 +0700

    belajar-spring-web-mvc: Testing memanggil API /person dengan perform POST

---
+++ src/test/java/bernadinusnaisau/spring/webmvc/controller/PersonControllerTest.java
@@ -0,0 +1,37 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PersonControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void createPerson() throws Exception {
+        mockMvc.perform(post("/person")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                        .param("firstName", "Bernadinus")
+                        .param("lastName", "Naisau")
+                        .param("email", "bernad@example.com")
+                        .param("phoneNumber", "021"))
+                .andExpectAll(
+                        status().isOk(),
+                        content().string(Matchers.containsString("Succesfully Create Person Bernadinus" +
+                                " Naisau " + " with email bernad@example.com" + " and phone number 021")
+                        ));
+    }
+
+}
