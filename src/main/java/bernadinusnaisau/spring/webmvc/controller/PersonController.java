bc09bf71f7caebd6d6b21666e049fbb21833cb55
bernad <bernadinusnaisau230803@gmail.com>
2025-09-08 20:35:42 +0700

    belajar-spring-web-mvc: Implementasi annotation @ModelAttribute untuk mengambil banyak parameter field dari suatu class

---
+++ src/main/java/bernadinusnaisau/spring/webmvc/controller/PersonController.java
@@ -0,0 +1,26 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import bernadinusnaisau.spring.webmvc.model.CreatePersonRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class PersonController {
+
+    @PostMapping(path = "/person", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public String createPerson(@ModelAttribute CreatePersonRequest request) {
+        return new StringBuilder().append("Succesfully Create Person ")
+                .append(request.getFirstName()).append(" ")
+                .append(request.getLastName()).append(" ")
+                .append(" with email ").append(request.getEmail())
+                .append(" and phone number ").append(request.getPhoneNumber())
+                .toString();
+    }
+}
