a23d2eb28e274a2499fc14c942c024e4aff32323
bernad <bernadinusnaisau230803@gmail.com>
2025-09-09 20:08:28 +0700

    belajar-spring-web-mvc: Implementasi Object BindingResult untuk menampung error yang terjadi didalam validation error. Seleain validation error tidak akan ditampung dalam BindingResult

--- src/main/java/bernadinusnaisau/spring/webmvc/controller/PersonController.java
+++ src/main/java/bernadinusnaisau/spring/webmvc/controller/PersonController.java
@@ -4,11 +4,16 @@ import bernadinusnaisau.spring.webmvc.model.CreatePersonRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 @Controller
 public class PersonController {
@@ -16,9 +21,20 @@ public class PersonController {
         @PostMapping(path = "/person", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
         @ResponseBody
         @ResponseStatus(HttpStatus.OK)
-        public String createPerson(@ModelAttribute @Valid CreatePersonRequest request) {
+        public ResponseEntity<String> createPerson(@ModelAttribute @Valid CreatePersonRequest request,
+                                   BindingResult bindingResult) {
+
+                List<FieldError> allErrors = bindingResult.getFieldErrors();
+                if (!allErrors.isEmpty()) {
+                        allErrors.forEach(fieldError -> {
+                                System.out.println(fieldError.getField() + ": " + fieldError.getDefaultMessage());
+                        });
+                        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("You Send Invalid Data");
+                }
+
                 System.out.println(request);
-            return new StringBuilder()
+
+                String response = new StringBuilder()
                     .append("Succesfully Create Person ")
                     .append(request.getFirstName()).append(" ")
                     .append(request.getLastName()).append(" ")
@@ -29,5 +45,6 @@ public class PersonController {
                     .append(request.getAddress().getCity())
                     .append(request.getAddress().getState())
                     .toString();
+                return ResponseEntity.ok(response);
         }
 }
