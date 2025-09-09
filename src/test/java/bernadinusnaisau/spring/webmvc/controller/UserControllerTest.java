8026a17ed0bce15dbc16fae4b6a60beda46fd94f
bernad <bernadinusnaisau230803@gmail.com>
2025-09-09 21:30:19 +0700

    belajar-spring-web-mvc: Testing ModelAttribute

---
+++ src/test/java/bernadinusnaisau/spring/webmvc/controller/UserControllerTest.java
@@ -0,0 +1,29 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import bernadinusnaisau.spring.webmvc.model.User;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getUser() throws Exception {
+        mockMvc.perform(get("/user/current")
+                .sessionAttr("user", new User("admin")))
+                .andExpectAll(status().isOk(),
+                        content().string(Matchers.containsString("Hello :admin")));
+    }
+}
