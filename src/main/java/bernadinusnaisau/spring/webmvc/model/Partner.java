6288bf4b8497866ca22b18b6880fe1ba8b88a1c9
bernad <bernadinusnaisau230803@gmail.com>
2025-09-10 18:54:09 +0700

    belajar-spring-web-mvc : Model untuk implementasi Argument Resolver

---
+++ src/main/java/bernadinusnaisau/spring/webmvc/model/Partner.java
@@ -0,0 +1,13 @@
+package bernadinusnaisau.spring.webmvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Partner {
+    private String id;
+    private String name;
+}
