16cda7c5a33dc287747bf33c9af88007c290d65d
bernad <bernadinusnaisau230803@gmail.com>
2025-09-10 19:09:29 +0700

    belajar-spring-web-mvc : Implementasi Static Resource

---
+++ src/test/java/bernadinusnaisau/spring/webmvc/controller/StaticTest.java
@@ -0,0 +1,29 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StaticTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getStatic() throws Exception {
+        mockMvc.perform(get("/index.html"))
+                .andExpectAll(status().isOk(),
+                        content().string(Matchers.containsString("Hello Static")));
+    }
+}
