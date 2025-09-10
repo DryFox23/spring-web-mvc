77244670a2c6217d67f7bcb93ea357ff1df901ba
bernad <bernadinusnaisau230803@gmail.com>
2025-09-10 19:43:16 +0700

    belajar-spring-web-mvc : Implementasi object ModelAndView pada controllernya

---
+++ src/test/java/bernadinusnaisau/spring/webmvc/controller/HelloModelAndViewTest.java
@@ -0,0 +1,31 @@
+package bernadinusnaisau.spring.webmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloModelAndViewTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getHelloModelAndView() throws Exception {
+        mockMvc.perform(get("/web/hello").queryParam("name", "Bernad"))
+                .andExpectAll(
+                        status().isOk(),
+                        content().string(Matchers.containsString("<title>Hello Page</title>")),
+                        content().string(Matchers.containsString("hello Bernad"))
+                );
+
+    }
+}
