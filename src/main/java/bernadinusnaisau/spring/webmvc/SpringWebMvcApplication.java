f7b930ac15bc3e65f6c544701a94211ca39b8d70
bernad <bernadinusnaisau230803@gmail.com>
2025-09-11 21:06:58 +0700

    belajar-spring-web-mvc : Implementasi RestTemplate dengan RestTemplateBuilder

---
+++ src/main/java/bernadinusnaisau/spring/webmvc/SpringWebMvcApplication.java
@@ -0,0 +1,34 @@
+package bernadinusnaisau.spring.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+
+
+@SpringBootApplication
+public class SpringWebMvcApplication {
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder
+				.requestFactory(() -> {
+					var factory = new SimpleClientHttpRequestFactory();
+					factory.setConnectTimeout(10_000);
+					factory.setReadTimeout(10_000);
+					return factory;
+				})
+				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcApplication.class, args);
+	}
+
+}
